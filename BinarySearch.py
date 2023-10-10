7ef00801f1ed1a3bec7032c76f0cf2cec7201da5
Ankita Priya <45851363+ankita2514@users.noreply.github.com>
2023-10-10 17:04:34 +0530

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,18 @@
+def BinarySearch(arr,target):
+    low = 0
+    high = len(arr) - 1
+    while low<=high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid]<target:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+
+print(BinarySearch(nums, target))
